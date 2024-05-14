411b15307d82b9e011d8d7d7fec33bb25473f49e
caman007 <101483730+caman007@users.noreply.github.com>
2024-05-14 19:36:59 +0500

    database connection hw1.py

--- scripts/hw1.py
+++ scripts/hw1.py
@@ -1,1 +1,17 @@
+import csv
+import requests
+import pymongo
+import mysql.connector
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = client["parking_violations"] 
+collection = database["violations"] 
+
+# Connect to MySQL
+connection = mysql.connector.connect(
+    host="localhost",
+    user="user",
+    password="pass"
+)
 

15d324d11d62b4f5bbce0358b3711416c3113777
caman007 <101483730+caman007@users.noreply.github.com>
2024-05-14 23:47:02 +0500

    Final Update

--- scripts/hw1.py
+++ scripts/hw1.py
@@ -11,11 +11,9 @@ collection = database["violations"]
 # Connect to MySQL
 connection = mysql.connector.connect(
     host="localhost",
-    user="user",
-    password="pass"
+    user="root",
+    password="654239"
 )
-cursor = connection.cursor()
-
 
 with requests.Session() as s:
     raw_data = s.get("https://data.cityofnewyork.us/resource/nc67-uf89.csv?$limit=50000&$offset=0")
@@ -32,104 +30,89 @@ documents = [{header: row[idx] for idx, header in enumerate(headers)} for row in
 collection.insert_many(documents)
 print("Data Inserted in MongoDB Successfully.")
 
+cursor = connection.cursor()
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS violations")
 cursor.execute("USE violations")
 
-dimension_schemas = {
-    
-    "state": "state VARCHAR(255)",
-    "license_type": "license_type VARCHAR(255)",
-    "violation": "Violation VARCHAR(255)",
-    "precinct": "precinct VARCHAR(255)",
-    "county": "county VARCHAR(255)",
-    "issuing_agency": "issuing_agency VARCHAR(255)",
-    "violation_status": "violation_status VARCHAR(255)"
-}
 
-for dimension, schema in dimension_schemas.items():
-    create_dimension_table_query = f"CREATE TABLE IF NOT EXISTS {dimension} ({dimension}ID INT AUTO_INCREMENT PRIMARY KEY, {schema})"
-    cursor.execute(create_dimension_table_query)
-    connection.commit()
-
-
-fact_table_schema = (
+dimension_table_schema=(
+    "plate VARCHAR(255), "
+    "state VARCHAR(255), "
+    "license_type VARCHAR(255), "
+    "violation VARCHAR(255), "
     "summons_number VARCHAR(255), "
+    "precinct VARCHAR(255), "
+    "county VARCHAR(255), "
+    "issuing_agency VARCHAR(255), "
+    "violation_status VARCHAR(255), "
     "issue_date VARCHAR(255), "
     "violation_time VARCHAR(255), "
-    "violation VARCHAR(255), "
     "judgment_entry_date VARCHAR(255), "
+    "summons_image VARCHAR(255) "
+)
+
+fact_table_schema = (
     "fine_amount VARCHAR(255), "
     "penalty_amount VARCHAR(255), "
     "interest_amount VARCHAR(255), "
     "reduction_amount VARCHAR(255), "
     "payment_amount VARCHAR(255), "
     "amount_due VARCHAR(255), "
-    "precinct VARCHAR(255), "
-    "county VARCHAR(255), "
-    "issuing_agency VARCHAR(255), "
-    "violation_status VARCHAR(255), "
-    "summons_image VARCHAR(255), "
-    "countyID INT, "
-    "issuing_agencyID INT, "
-    "stateID INT, " 
-    "violationID INT, "
-    "precinctID INT, "
-    "license_typeID INT, "
-    "violation_statusID INT"
+    "incidentID INT "
 )
 
+# create dimension table
+create_dimension_table_query = f"CREATE TABLE IF NOT EXISTS incident (incidentID INT AUTO_INCREMENT PRIMARY KEY, {dimension_table_schema})"
+cursor.execute(create_dimension_table_query)
+connection.commit()
+print("Dimensional Table Created")
+
 # Create fact table
-create_fact_table_query = f"CREATE TABLE IF NOT EXISTS traffic_violations ({fact_table_schema})"
+create_fact_table_query = f"CREATE TABLE IF NOT EXISTS penalty ({fact_table_schema})"
 cursor.execute(create_fact_table_query)
 connection.commit()
-
+print("Fact Table Created")
 
 fact_table_columns = [
-    "summons_number VARCHAR(255), ",
-    "issue_date VARCHAR(255), ",
-    "violation_time VARCHAR(255), ",
-    "violation VARCHAR(255), ",
-    "judgment_entry_date VARCHAR(255), ",
     "fine_amount VARCHAR(255), ",
     "penalty_amount VARCHAR(255), ",
     "interest_amount VARCHAR(255), ",
     "reduction_amount VARCHAR(255), ",
     "payment_amount VARCHAR(255), ",
     "amount_due VARCHAR(255), ",
+    "incidentID INT, "
+]
+
+dimension_table_columns = [
+    "plate VARCHAR(255), ",
+    "state VARCHAR(255), ",
+    "license_type VARCHAR(255), ",
+    "violation VARCHAR(255), ",
+    "summons_number VARCHAR(255), ",
     "precinct VARCHAR(255), ",
     "county VARCHAR(255), ",
     "issuing_agency VARCHAR(255), ",
     "violation_status VARCHAR(255), ",
-    "summons_image VARCHAR(255)",
+    "issue_date VARCHAR(255), ",
+    "violation_time VARCHAR(255), ",
+    "judgment_entry_date VARCHAR(255), ",
+    "summons_image VARCHAR(255), "
 ]
-
 
 counter = 1
 for row in documents:
-    query = f"INSERT INTO state (state) VALUES (%s)"
-    cursor.execute(query,(row["state"],))
 
-    query = f"INSERT INTO license_type (license_type) VALUES (%s)"
-    cursor.execute(query,(row["license_type"],))
+
+    dim_columns = ", ".join([item.split(" ")[0] for item in dimension_table_columns]) 
+    dim_query = f"INSERT INTO incident ({dim_columns}) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(dim_query,([row[item.split(" ")[0]] for item in dimension_table_columns]))
+    print("dim inserted")
+
+    fact_columns = ", ".join([item.split(" ")[0] for item in fact_table_columns]) 
+    fact_query = f"INSERT INTO penalty ({fact_columns}) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(fact_query,([row[item.split(" ")[0]] for item in fact_table_columns[:-1]]+[counter]))
+    print("fact inserted")
     
-    query = f"INSERT INTO violation (violation) VALUES (%s)"
-    cursor.execute(query,(row["violation"],))
-
-    query = f"INSERT INTO precinct (precinct) VALUES (%s)"
-    cursor.execute(query,(row["precinct"],))
-
-    query = f"INSERT INTO county (county) VALUES (%s)"
-    cursor.execute(query,(row["county"],))
-
-    query = f"INSERT INTO issuing_agency (issuing_agency) VALUES (%s)"
-    cursor.execute(query,(row["issuing_agency"],))
-
-    query = f"INSERT INTO violation_status (violation_status) VALUES (%s)"
-    cursor.execute(query,(row["violation_status"],))
-
-    columns = ", ".join([item.split(" ")[0] for item in fact_table_columns]+ ["countyID", "issuing_agencyID", "stateID", "violationID", "precinctID", "license_typeID", "violation_statusID"]) 
-    query = f"INSERT INTO traffic_violations ({columns}) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(query,([row[item.split(" ")[0]] for item in fact_table_columns]+[counter,counter,counter,counter,counter,counter,counter]))
     connection.commit()
     counter +=1
